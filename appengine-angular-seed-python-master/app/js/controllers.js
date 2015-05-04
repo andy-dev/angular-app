9c373104d297f80cc5cadd0671f51aca986e9512
andy-dev <andres.macedo@gmail.com>
2015-05-04 13:52:46 -0700

    add db setup with firebase, needs styling

--- appengine-angular-seed-python-master/app/js/controllers.js
+++ appengine-angular-seed-python-master/app/js/controllers.js
@@ -6,11 +6,29 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function(){
 
   }])
-  .controller('WaitListController', ['$scope',function($scope) {
-    $scope.parties = [];
+  .controller('WaitListController', ['$scope','$firebase',function($scope, $firebase) {
+
+    var partiesRef = new Firebase('https://waitandeat-andydev.firebaseio.com/');
+
+    $scope.parties = $firebase(partiesRef);
+
     $scope.party = {name: '', phone: '', size: ''};
+
     $scope.saveParty = function(){
-      $scope.parties.push($scope.party);
+      $scope.parties.$add($scope.party);
       $scope.party = {name: '', phone: '', size: ''};
     };
   }])
+
+
+// instead of being an array will conect it to our firebase service
+// takes a parameter that is reference to our parties
+// we are going to use add instead of push, will add data to our firebase
+// $scope.parties.push($scope.party);
+// $scope.parties is now a firebase object
+
+// inject firebase service into our controller
+// instead of an array we have an object
+// now we have something diff we have a key and all of this {"-JoVgC_7T0NnIemxEIm8":{"name":"andy","phone":"3462345","size":"4
+// if we go to our firebase dashboard, we can see we have two items,
+// if we delete all of our data, it goes away in our app too
