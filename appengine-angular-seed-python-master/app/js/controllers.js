eefc3d88b796ba273cdbabfd4e141d1e5d62cbf7
andy-dev <andres.macedo@gmail.com>
2015-05-03 21:48:31 -0700

    add waitlist partial, still not persistent, needs db implementation

--- appengine-angular-seed-python-master/app/js/controllers.js
+++ appengine-angular-seed-python-master/app/js/controllers.js
@@ -6,9 +6,11 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function(){
 
   }])
-  .controller('MyCtrl1', [function() {
-
+  .controller('WaitListController', ['$scope',function($scope) {
+    $scope.parties = [];
+    $scope.party = {name: '', phone: '', size: ''};
+    $scope.saveParty = function(){
+      $scope.parties.push($scope.party);
+      $scope.party = {name: '', phone: '', size: ''};
+    };
   }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
