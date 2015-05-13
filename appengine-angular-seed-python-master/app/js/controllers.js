046deff3909171d02b405489130147828fa6894a
andy-dev <andres.macedo@gmail.com>
2015-05-12 18:41:41 -0700

    took notes from code to evernote

--- appengine-angular-seed-python-master/app/js/controllers.js
+++ appengine-angular-seed-python-master/app/js/controllers.js
@@ -46,18 +46,3 @@ angular.module('myApp.controllers', [])
   }]);
 
 
-// instead of being an array will conect it to our firebase service
-// takes a parameter that is reference to our parties
-// we are going to use add instead of push, will add data to our firebase
-// $scope.parties.push($scope.party);
-// $scope.parties is now a firebase object
-
-// inject firebase service into our controller
-// instead of an array we have an object
-// now we have something diff we have a key and all of this {"-JoVgC_7T0NnIemxEIm8":{"name":"andy","phone":"3462345","size":"4
-// if we go to our firebase dashboard, we can see we have two items,
-// if we delete all of our data, it goes away in our app too
-
-// you could do $scope.authService = authService
-// and then you would have ng-click = authService.logout() and have a very simple app
-// but its better for sperate methods in the view
